eec9865cf0ec6c43094f4bad6db332fd260c9da2
Sun_Gll <gll71103@163.com>
2019-05-30 22:25:56 +0800

    5.30日练习

---
+++ 二叉树中序遍历(递归).java
@@ -0,0 +1,25 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ class Solution {
+     List<Integer> list = new ArrayList<>();
+
+     public List<Integer> inorderTraversal(TreeNode root) {
+
+         if(null != root) {
+             inorderTraversal(root.left);
+             list.add(root.val);
+             inorderTraversal(root.right);
+         }
+
+         return list;
+     }
+ }
