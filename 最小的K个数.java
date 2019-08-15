176327981a53154195369fadad59a56bdab0a44d
Sun_Gll <gll71103@163.com>
2019-08-15 23:00:16 +0800

    8.15日练习

---
+++ 最小的K个数.java
@@ -0,0 +1,20 @@
+输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+
+```java
+import java.util.*;
+
+public class Solution {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        if(k>0 && k<=input.length) {
+            Arrays.sort(input);
+            for(int i = 0;i<k;++i) {
+                list.add(input[i]);
+            }
+        }
+        
+        return list;
+    }
+}
+```
